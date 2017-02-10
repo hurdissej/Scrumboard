6ecc9f1266145bcaec937571ecf5cfa9df0c177b
hurdissej <hurdissej@me.com>
2017-02-10 14:20:15 +0000

    stuff

---
+++ djangular/scrumboard/static/scrumboard/scrumboard.js
@@ -0,0 +1,34 @@
+/**
+ * Created by elliot.hurdiss on 08/02/2017.
+ */
+
+(function () {
+    'use strict';
+    angular.module('scrumboard.demo', [])
+        .controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
+    function ScrumboardController($scope, $http) {
+        $scope.add = function (list, title) {
+            var card = {
+                list: list.id,
+                title: title
+            };
+            $http.post('/scrumboard/cards/', card)
+                .then(function(response) {
+                    list.cards.push(response.data);
+                },
+                function () {
+                    alert("Could not create Card")
+                });
+
+
+        };
+        $scope.data = [];
+
+        $http.get('/scrumboard/lists/').then(function(response) {
+            $scope.data = response.data;
+        });
+
+
+    }
+
+}());
